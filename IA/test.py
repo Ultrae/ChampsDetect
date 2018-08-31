76a1fab9c18c4e6229c92b2c585fec6787882391
Corentin <corentin.mounier@epita.fr>
2018-08-31 22:53:09 +0200

    [TEST] add tiff image test

--- IA/test.py
+++ IA/test.py
@@ -1,6 +1,6 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-img = load_img('./wallpaper.png')
+img = load_img('./450nm.tiff')
 x = img_to_array(img)
 print (x)
 print (len(x[0]))
