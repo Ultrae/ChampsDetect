2e7fef3697e17717d736ac925ad4545a9efb399e
ghirla_m <mathieu.ghirlanda@epita.fr>
2018-09-01 20:43:59 +0200

    [WEBSITE] Add dashboard

---
+++ website/dashboard/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    return render(request, 'dashboard/index.html')
