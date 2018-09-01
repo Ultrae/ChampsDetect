74d25df888c1b997b0c272411dd5b21c4a714c63
Victoria Guehennec <victoria.guehennec@epita.fr>
2018-09-01 08:08:56 +0200

    [USER] Basic form with Django

--- website/user/views.py
+++ website/user/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
 
+from .forms import UserForm
 
 def index(request):
     return render(request, 'user/index.html')
+
+def get_name(request):
+    if request.method == 'POST':
+        print("IF")
+        form = UserForm(request.POST)
+        if form.is_valid():
+            return render(request, 'user/index.html')
+    else:
+
+        print("ELSE")
+        form = UserForm()
+    return render(request, 'user/index.html', {'form': form})
