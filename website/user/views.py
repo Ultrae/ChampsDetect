6510dd3cb144852e7c697ff8d5e0f0d1a99e63c5
Victoria Guehennec <victoria.guehennec@epita.fr>
2018-09-01 10:10:22 +0200

    [USER] zip extractor for User form

--- website/user/views.py
+++ website/user/views.py
@@ -4,14 +4,21 @@ from django.template import loader
 
 from django.views.decorators.csrf import csrf_exempt
 
+import zipfile
+
 from .forms import UserForm
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        form = UserForm(request.POST)
+        form = UserForm(request.POST, request.FILES)
         if form.is_valid():
+            # Save the folder
+            img_zip = zipfile.ZipFile(request.FILES['img_folder'])
+            img_zip.extractall('zip_folders/')
+            img_zip.close()
+
             return render(request, 'user/index.html')
     else:
         form = UserForm()
-    return render(request, 'user/index.html', {'form': form})
+    return render(request, 'user/index.html', { 'form': form })
