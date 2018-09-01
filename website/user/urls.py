1938be42b8262c413a2d59671f95b3d31f457ad7
Victoria Guehennec <victoria.guehennec@epita.fr>
2018-09-01 08:21:02 +0200

    [USER] Put the form on User's index page

--- website/user/urls.py
+++ website/user/urls.py
@@ -6,5 +6,4 @@ app_name = 'user'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('your-name', views.get_name, name='your-name')
 ]
--- website/user/views.py
+++ website/user/views.py
@@ -2,19 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template import loader
 
+from django.views.decorators.csrf import csrf_exempt
+
 from .forms import UserForm
 
+@csrf_exempt
 def index(request):
-    return render(request, 'user/index.html')
-
-def get_name(request):
     if request.method == 'POST':
-        print("IF")
         form = UserForm(request.POST)
         if form.is_valid():
             return render(request, 'user/index.html')
     else:
-
-        print("ELSE")
         form = UserForm()
     return render(request, 'user/index.html', {'form': form})
