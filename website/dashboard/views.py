4b82f9d50b1d1f31f18575f2e63b2b70c84371e3
ghirla_m <mathieu.ghirlanda@epita.fr>
2018-09-02 15:10:23 +0200

    [WEBSITE] Add db to dashboard

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from user.models import Log
-
+from django.db.models import Avg
 
 def index(request):
     data = Log.objects.all()
-    return render(request, 'dashboard/index.html', {'data': data})
+    data2 = Log.objects.all().aggregate(Avg('pourcent'))
+    data2 = data2['pourcent__avg']
+    return render(request, 'dashboard/index.html',
+                  {'data': data, 'data2': data2})
--- website/user/models.py
+++ website/user/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Log(models.Model):
     date = models.DateTimeField('date published')
+    description = models.CharField(max_length=500, default="")
     pourcent = models.IntegerField(default=0)
