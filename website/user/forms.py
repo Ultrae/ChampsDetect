d8c9238e24c53cda1a2ce48e7df69c0722102b52
Victoria Guehennec <victoria.guehennec@epita.fr>
2018-09-02 11:46:16 +0200

    [USER] Select type of picture

--- website/user/forms.py
+++ website/user/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 class UserForm(forms.Form):
     img_folder = forms.FileField()
-    select_list = ["Seau de raisin"]
-    select = forms.ChoiceField(choices=select_list)
+    select_list = [(1, "Seau de raisin"), (2, "Parcelle de blé")]
+    select = forms.ChoiceField(choices=select_list,
+                               initial='',
+                               widget=forms.Select())
